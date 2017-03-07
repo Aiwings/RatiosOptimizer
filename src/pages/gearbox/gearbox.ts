cce2fceef45d1cccd8192a0db505b25ae51f9317
Guillaume Roux <roux.gensou@gmail.com>
2017-03-07 14:12:21 +0100

    MacOS cleared gearboxes table

--- src/pages/gearbox/gearbox.ts
+++ src/pages/gearbox/gearbox.ts
@@ -1,18 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import { NavController, ModalController,ToastController } from 'ionic-angular';
-import { CarProvider } from '../../providers/car-provider';
-import { GearProvider } from '../../providers/gear-provider';
-import { RatioModalComponent } from '../../components/ratio-modal/ratio-modal';
-import { Gearbox } from '../../app/gearbox';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import {
+  NavController,
+  ModalController,
+  ToastController
+} from 'ionic-angular';
+import {
+  CarProvider
+} from '../../providers/car-provider';
+import {
+  GearProvider
+} from '../../providers/gear-provider';
+import {
+  RatioModalComponent
+} from '../../components/ratio-modal/ratio-modal';
+import {
+  Gearbox
+} from '../../app/gearbox';
 @Component({
   selector: 'page-gearbox',
   templateUrl: 'gearbox.html'
 })
 export class GearboxPage implements OnInit {
 
-
   types = ["DG", "DGB", "FG400", "FGA", "FT1", "FT200", "LD", "LG400", "LG500", "MK5", "MK6", "MK8"];
-  gearBoxes : Gearbox [] = [];
+  gearBoxes: Gearbox[] = [];
   selectedGear: Gearbox;
   constructor(
     public navCtrl: NavController,
@@ -25,74 +39,69 @@ export class GearboxPage implements OnInit {
 
   public gb: Gearbox;
 
-    toastsuccess = this.toastCtrl.create({
-        message :'',
-        position:'bottom',
-        duration:3000
-      });
+  toastsuccess = this.toastCtrl.create({
+    message: '',
+    position: 'bottom',
+    duration: 3000
+  });
 
-      toasterror = this.toastCtrl.create({
-        message :'',
-        position:'bottom',
-       showCloseButton:true,
-       closeButtonText:'ok'
-      });
+  toasterror = this.toastCtrl.create({
+    message: '',
+    position: 'bottom',
+    showCloseButton: true,
+    closeButtonText: 'ok'
+  });
 
   ngOnInit(): void {
 
-      this.gb = {
-        id: 0,
-        carid: this.carProv.getSelectedCar().id,
-        type: "",
-        brand: "",
-        serial:0,
-      };
-      this.gearBoxes.push(this.gb);
-      this.gearProv.setGB(this.gb);
-       this.gearProv.getGBs(this.carProv.getSelectedCar().id).then((gbs)=>{
-           if(gbs)
-           {
-            this.gearBoxes = gbs;
-            console.log("GBS " +JSON.stringify(gbs));
-           }
-         
-      }).catch((error)=>{
-        console.error(error.message,error);
-          this.toasterror.setMessage("Impossible de récupérer les boites \n "+error.message);
-          this.toasterror.present();
-      });
+    this.gb = {
+      id: 0,
+      carid: this.carProv.getSelectedCar().id,
+      type: "",
+      brand: "",
+      serial: 0,
+    };
+    this.gearProv.setGB(this.gb);
+    this.gearProv.getGBs(this.carProv.getSelectedCar().id).then((gbs) => {
+      if (gbs) {
+        this.gearBoxes = gbs;
+        console.log("GBS " + JSON.stringify(gbs));
+      }
+
+    }).catch((error) => {
+      console.error(error.message, error);
+      this.toasterror.setMessage("Impossible de récupérer les boites \n " + error.message);
+      this.toasterror.present();
+    });
   }
-  onSelect():void {
-    if(this.selectedGear)
-    {
+  onSelect(): void {
+    if (this.selectedGear) {
       this.gb = this.selectedGear;
     }
   }
 
   save(): void {
-    this.gearProv.saveGB(this.gb).then((data)=>
-    {
-        this.gearProv.getGB(this.carProv.getSelectedCar().id).then((gb)=>{
-          this.gb =gb;
-          this.gearBoxes.push(this.gb);
-        }).catch((error)=>{
-           this.toasterror.setMessage("Il y a eu une erreur lors de la sauvegarde \n "+error.message);
-           this.toasterror.present();
-        });
+    this.gearProv.saveGB(this.gb).then((data) => {
+      this.gearProv.getGB(this.carProv.getSelectedCar().id).then((gb) => {
+        this.gb = gb;
+        this.gearBoxes.push(this.gb);
+      }).catch((error) => {
+        this.toasterror.setMessage("Il y a eu une erreur lors de la sauvegarde \n " + error.message);
+        this.toasterror.present();
+      });
       this.toastsuccess.setMessage("Succès de la sauvegarde");
       this.toastsuccess.present();
-      
-    }).catch((error)=>{
-           this.toasterror.setMessage("Il y a eu une erreur lors de la sauvegarde \n "+error.message);
-           this.toasterror.present();
+
+    }).catch((error) => {
+      this.toasterror.setMessage("Il y a eu une erreur lors de la sauvegarde \n " + error.message);
+      this.toasterror.present();
     })
   }
 
   ratios(): void {
-    
-    let modal = this.modalCtrl.create(RatioModalComponent,{});
+
+    let modal = this.modalCtrl.create(RatioModalComponent, {});
     modal.present();
-    modal.onDidDismiss((data)=>{
-    });
+    modal.onDidDismiss((data) => {});
   }
 }
